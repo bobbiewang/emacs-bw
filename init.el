5afddb6d8be9b383cfcb3df698760e192cb2ef49
Bo Wang <Bobbie.Wang@gmail.com>
2017-10-31 13:41:35 +0800

    通过 Org Babel 加载 modules 文件。

--- init.el
+++ init.el
@@ -7,7 +7,13 @@
 (setq package-enable-at-startup nil)    ; 不在 init 文件加载后重复初始化
 (package-initialize)
 
-;; 通过 Org Babel 加载初始化文件
+;; 通过 Org Babel 加载 core 文件
 (require 'ob-tangle)
 (dolist (pkg '(infrastructure packages ui misc keybindings))
   (org-babel-load-file (locate-user-emacs-file (format "core/config-%s.org" pkg))))
+
+;; 通过 Org Babel 加载 modules 文件
+(setq bw/modules-dir (locate-user-emacs-file "modules/"))
+(when (file-exists-p bw/modules-dir)
+  (message "Loading configuration files of modules...")
+  (mapc 'org-babel-load-file (directory-files bw/modules-dir 't "config-.*org$")))
