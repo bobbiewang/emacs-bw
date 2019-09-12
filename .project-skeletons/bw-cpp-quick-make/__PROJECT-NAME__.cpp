bf8b359e03f75e359c78b6007345ee8d4960bc29
Bo Wang <Bobbie.Wang@gmail.com>
2019-09-12 23:13:08 +0800

    使用 skeletor 并创建 C++ Quick Executable - Makefile 模板。

---
+++ .project-skeletons/bw-cpp-quick-make/__PROJECT-NAME__.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
