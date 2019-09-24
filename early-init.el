fbe56e0416343dfbd2a05cb7957f32efbb1f3f5e
Bo Wang <Bobbie.Wang@gmail.com>
2019-09-25 00:55:04 +0800

    整理扩展的临时文件到 .emacs.d/var，扩展的配置文件到 .emacs.d/etc。

--- early-init.el
+++ early-init.el
@@ -1,2 +1,2 @@
-(setq package-user-dir (locate-user-emacs-file ".elpa"))
-(setq nsm-settings-file (locate-user-emacs-file ".network-security.data"))
+(setq package-user-dir (locate-user-emacs-file "var/elpa"))
+(setq nsm-settings-file (locate-user-emacs-file "var/network-security.data"))
--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(setq package-user-dir (locate-user-emacs-file ".elpa"))
+(setq package-user-dir (locate-user-emacs-file "var/elpa"))
 (setq package-enable-at-startup nil)    ; 不在 init 文件加载后重复初始化
 (package-initialize)
 
