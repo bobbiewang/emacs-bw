22301942bea72ac9c093257d6434759dbd340b72
Bo Wang <Bobbie.Wang@gmail.com>
2019-10-10 21:41:59 +0800

    使用 emacs-china 镜像。

--- init.el
+++ init.el
@@ -5,7 +5,9 @@
 ;; 初始化 ELPA 环境
 
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+;; 使用 emacs-china 镜像替代 http://melpa.org/packages/
+(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+			             ("melpa" . "http://elpa.emacs-china.org/melpa/")))
 (setq package-user-dir (locate-user-emacs-file "var/elpa"))
 (setq package-enable-at-startup nil)    ; 不在 init 文件加载后重复初始化
 (package-initialize)
