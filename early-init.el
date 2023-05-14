96e5f1133b54bfd2e9bc7e50e9fdf4527449df91
Bo Wang <Bobbie.Wang@gmail.com>
2023-05-14 12:15:25 +0800

    自定义 eln-cache 的路径。

--- early-init.el
+++ early-init.el
@@ -1,2 +1,6 @@
 (setq package-user-dir (locate-user-emacs-file "var/elpa"))
+
 (setq nsm-settings-file (locate-user-emacs-file "var/network-security.data"))
+
+(when (boundp 'native-comp-eln-load-path)
+  (startup-redirect-eln-cache (expand-file-name "var/eln-cache/" user-emacs-directory)))
