8e7269361e960a112f55d547c5142c47507ad7e3
Bo Wang <Bobbie.Wang@gmail.com>
2024-10-27 18:06:00 +0800

    调整 gc-cons-threshold。

--- early-init.el
+++ early-init.el
@@ -1,3 +1,9 @@
+;; 缺省 800K 就进行 GC。
+;; - 启动时设置为最大值，优化启动速度
+;; - 启动完成后修改为 100M，减少卡顿
+(setq gc-cons-threshold most-positive-fixnum)
+(add-hook 'after-init-hook #'(lambda () (setq gc-cons-threshold 100000000)))
+
 (setq package-user-dir (locate-user-emacs-file "var/elpa"))
 
 (setq nsm-settings-file (locate-user-emacs-file "var/network-security.data"))
